b788d72c04b949868daca705d03689faeb4b5e0b
Nathaniel Fitzenrider <16159914+nfitzen@users.noreply.github.com>
2019-02-11 00:23:37 -0500

    Apparently idk how to program JS

--- like-tweet-generator/generate-tweet.js
+++ like-tweet-generator/generate-tweet.js
@@ -1,5 +1,5 @@
 function open_tweet_dialog(videoURL, apiKey, newtab = true) {
-    let videoId = videoURL.extract().v
+    let videoId = videoURL.extract().v;
     $.ajax({
         url: "https://www.googleapis.com/youtube/v3/videos?id=" + videoId + "&key="+ apiKey + "&fields=items(snippet(title))&part=snippet",
         dataType: "jsonp",
@@ -7,7 +7,11 @@ function open_tweet_dialog(videoURL, apiKey, newtab = true) {
             let title = data.items[0].snippet.title;
             if (title.length > 231) title = title.slice(0, 229) + "...";
             let tweetURL = "https://twitter.com/intent/tweet?text=I+liked+a+%40YouTube+video+https%3A%2F%2Fyoutu.be%2F" + videoId + "+" + encodeURIComponent(title);
-            if (newtab === true) window.open(tweetURL) else window.location = tweetURL;
+            if (newtab === true) {
+                window.open(tweetURL);
+            } else {
+                window.location = tweetURL;
+            }
         },
         error: function(jqXHR, textStatus, errorThrown) {
             alert (textStatus, + ' | ' + errorThrown);
