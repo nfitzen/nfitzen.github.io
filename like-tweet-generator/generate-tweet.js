189734ef6f791ef1759da0cefa2de5af7dedd7fd
Nathaniel Fitzenrider <16159914+nfitzen@users.noreply.github.com>
2019-02-11 00:15:15 -0500

    Use proper JS & add jQuery CDN as Jekyll variable

--- like-tweet-generator/generate-tweet.js
+++ like-tweet-generator/generate-tweet.js
@@ -1,19 +1,13 @@
 function open_tweet_dialog(videoURL, apiKey, newtab = true) {
-    var videoId = videoURL.extract().v
+    let videoId = videoURL.extract().v
     $.ajax({
-        url: "https://www.googleapis.com/youtube/v3/videos?id=" + videoId + "&key="+ apiKey + "&fields=items(snippet(title))&part=snippet", 
+        url: "https://www.googleapis.com/youtube/v3/videos?id=" + videoId + "&key="+ apiKey + "&fields=items(snippet(title))&part=snippet",
         dataType: "jsonp",
         success: function(data){
-            var title = data.items[0].snippet.title
-            if (title.length > 231) {
-                title = title.slice(0, 229) + "..."
-            }
-            var tweetURL = "https://twitter.com/intent/tweet?text=I+liked+a+%40YouTube+video+https%3A%2F%2Fyoutu.be%2F" + videoId + "+" + encodeURIComponent(title);
-            if (newtab === true) {
-                window.open(tweetURL);
-            } else {
-                window.location = tweetURL
-            }
+            let title = data.items[0].snippet.title;
+            if (title.length > 231) title = title.slice(0, 229) + "...";
+            let tweetURL = "https://twitter.com/intent/tweet?text=I+liked+a+%40YouTube+video+https%3A%2F%2Fyoutu.be%2F" + videoId + "+" + encodeURIComponent(title);
+            if (newtab === true) window.open(tweetURL) else window.location = tweetURL;
         },
         error: function(jqXHR, textStatus, errorThrown) {
             alert (textStatus, + ' | ' + errorThrown);
--- like-tweet-generator/index.md
+++ like-tweet-generator/index.md
@@ -1,9 +1,8 @@
 ---
 layout: default
 title: YT Tweeting
+jquery: true
 ---
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="dentist.min.js"></script>
 <script src="generate-tweet.js"></script>
 
 ### Tweet that you liked a YouTube video. Just paste the URL below and click Tweet me!
@@ -16,9 +15,8 @@ title: YT Tweeting
 Alternatively, drag this to your bookmarks (or right-click and select "bookmark this link"): <a href="javascript:(function(){window.open('https://nathaniel.fitzenrider.com/like-tweet-generator/?vidurl='+encodeURIComponent(document.URL))})();">Tweet me!</a>
 
 <script>
-var currentURL = document.URL;
-var params = currentURL.extract();
-if (typeof decodeURIComponent(params.vidurl) === 'string' || decodeURIComponent(params.vidurl) instanceof String) {
+let params = URLSearchParams(window.location.search);
+let vidurl = params.get('vidurl');
+if (typeof decodeURIComponent(vidurl) === 'string' || decodeURIComponent(vidurl) instanceof String)
     open_tweet_dialog(decodeURIComponent(params.vidurl), 'AIzaSyCWi1MRJNSoxmu4XsUFwJDBj01ND_vP76U', false);
-}
 </script>
